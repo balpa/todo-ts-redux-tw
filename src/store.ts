03e00ccd75343af9f3d678643c85ac8569fd5280
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-09-13 11:50:11 +0300

    didnt understand redu:xd

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
+import eventReducer from './store/events'
 
-export default configureStore({
-  reducer: {},
+export const store = configureStore({
+  reducer: {
+    events: eventReducer,
+  },
 })
+
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
--- src/store/events.ts
+++ src/store/events.ts
@@ -1,21 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit'
+import type { RootState } from '../store'
+
+interface EventText {
+  value: String,
+}
+const initialState: EventText = {
+  value: "",
+}
 
 export const events = createSlice({
   name: 'events',
-  initialState: {
-    value: "",
-  },
+  initialState,
   reducers: {
-    increment: (state) => {
+    addEventText: (state) => {
+
     },
-    decrement: (state) => {
-    },
-    incrementByAmount: (state, action) => {
+    deleteEventText: (state) => {
     },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = events.actions
-
+export const { addEventText, deleteEventText } = events.actions
+export const selectCount = (state: RootState) => state.events.value
 export default events.reducer
