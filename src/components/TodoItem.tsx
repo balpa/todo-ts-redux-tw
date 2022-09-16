8c028c0027710d5d4a017bcc346d09619edc9766
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-09-16 11:23:52 +0300

    todo-component scheme

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -7,12 +7,12 @@ interface PropTypes {
 const TodoItem = ({ color }: PropTypes) => {
 
   return (
-    <div className='w-full flex flex-row rounded-lg bg-[#C70039]'>
-      <div className='flex-1 w-32 rounded-md'>
-        sol
+    <div className='w-full flex flex-row rounded-lg bg-[#C70039] mt-5'>
+      <div className='w-4/5 rounded-md p-3 text-white'>
+        buraya todo metni gelecek ve iki üç satır olabilir ona bi bakalım diye uzun uzuuuuun yazıyorum satır olabilir ona bi bakalım diye uzun uzuuuuun yazıyorumsatır olabilir ona bi bakalım diye uzun uzuuuuun yazıyorum
       </div>
-      <div className='flex-2 w-32 rounded-md'>
-        sağ
+      <div className='w-1/5 rounded-r-md p-3 border-l-4'>
+        burada tamamlandı ya da beklet meklet zaman
       </div>
     </div>
   )
